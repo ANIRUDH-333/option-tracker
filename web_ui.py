36476dd3e474b7bc80e5b00d9222f5c0a8f5bee1
Anirudh Edpuganti <aedpuganti@guidewire.com>
2025-10-28 08:15:09 +0530

    Remove deployment files and Docker configuration

--- web_ui.py
+++ web_ui.py
@@ -143,5 +143,4 @@ if __name__ == '__main__':
     thread.start()
     
     # Start Flask server
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=5000)
